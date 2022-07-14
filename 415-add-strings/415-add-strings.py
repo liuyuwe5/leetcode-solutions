10297497afdbd20d5de4e0cdf7ed743a630dd542
oolonglilfox <94095961+oolonglilfox@users.noreply.github.com>
2022-07-14 17:26:40 +0800

    Time: 74 ms (19.87%), Space: 14.1 MB (8.58%) - LeetHub

--- 415-add-strings/415-add-strings.py
+++ 415-add-strings/415-add-strings.py
@@ -13,9 +13,6 @@ class Solution(object):
     
         for i in range(1, len1+1):
             if i <= len2:
-                #print("=============")
-                #print(num1[-i])
-                #print(num2[-i])
                 digitsum = ord(num1[-i]) + ord(num2[-i]) + prev - 2*ord("0") 
                 #print(digitsum)
                 if digitsum >= 10:
@@ -32,16 +29,10 @@ class Solution(object):
                 else:
                     prev = 0
                     cur = str(digitsum)
-            # print("digitsum: " + str(digitsum))
-            #print("cur: " + str(cur))
-            #print("res: " + str(res))
             res = cur + res
-            #print(res)
-             
                 
         i += 1
         while i <= len2:
-            #print(i)
             digitsum = ord(num2[-i]) + prev - ord("0") 
             if digitsum >= 10:
                 prev = 1

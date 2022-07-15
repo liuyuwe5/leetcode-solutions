48eb35756f7d8216958dcf7c6432fd024328c894
oolonglilfox <94095961+oolonglilfox@users.noreply.github.com>
2022-07-15 14:03:47 +0800

    Time: 8 ms (38.58%), Space: 42.9 MB (16.67%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.java
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -4,11 +4,7 @@ class Solution {
         HashMap<Character, Integer> charMap = new HashMap<Character, Integer>();
         for (int i=0; i < s.length(); i++) {
             Character c = s.charAt(i);
-            if (charMap.containsKey(c)) {
-                charMap.put(c, charMap.get(c)+1);
-            } else {
-                charMap.put(c, 1);
-            }
+            charMap.put(c, charMap.getOrDefault(c, 0)+1);
         }
         Integer cnt = 0;
         Integer numOfOdd = 0;

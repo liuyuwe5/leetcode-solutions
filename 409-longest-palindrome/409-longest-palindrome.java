f3271b3069192ca386745768004cc175d822d897
oolonglilfox <94095961+oolonglilfox@users.noreply.github.com>
2022-07-15 14:01:36 +0800

    Time: 7 ms (46.04%), Space: 42.8 MB (16.67%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+class Solution {
+    public int longestPalindrome(String s) {
+        HashMap<Character, Integer> charMap = new HashMap<Character, Integer>();
+        for (int i=0; i < s.length(); i++) {
+            Character c = s.charAt(i);
+            if (charMap.containsKey(c)) {
+                charMap.put(c, charMap.get(c)+1);
+            } else {
+                charMap.put(c, 1);
+            }
+        }
+        //System.out.println(charMap.values());
+        Integer cnt = 0;
+        Integer numOfOdd = 0;
+        for (Integer v: charMap.values()) {
+            if ((v & 1) == 0) {
+                cnt += v;
+            } else {
+                cnt += v - 1;
+                numOfOdd++;
+            }
+        }
+        if (numOfOdd >= 1) {
+            cnt++;
+        }
+        return cnt;
+    }
+    
+}
